0f1875b8fda0c9322fa6aa30220dd9f657ecbe3d
aikeru <aikeru@gmail.com>
2011-04-07 20:43:08 +0000

    Add reboot script James can download

--- jq4a.js
+++ jq4a.js
@@ -108,6 +108,22 @@
 			if(aOpts.error) { aOpts.error(e); }
 		}
 	}
+
+	$.reboot = function() {
+		var opsys = GetObject("winmgmts:{(Shutdown)}//./root/cimv2").ExecQuery("select * from Win32_OperatingSystem where Primary=true");
+		for(var enumItems = new Enumerator(opsys); !enumItems.atEnd(); enumItems.moveNext())
+		{
+			enumItems.item().Reboot();
+		}
+	}
+	$.shutdown = function() {
+		var opsys = GetObject("winmgmts:{(Shutdown)}//./root/cimv2").ExecQuery("select * from Win32_OperatingSystem where Primary=true");
+		for(var enumItems = new Enumerator(opsys); !enumItems.atEnd(); enumItems.moveNext())
+		{
+			enumItems.item().Reboot();
+		}
+	}
+
 	
 	context.$ = $;
  })(this);
