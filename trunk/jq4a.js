83e68b15e27f86ba3bbed8813d2b569cbedabc4f
aikeru@gmail.com <aikeru@gmail.com@e7c3be91-6012-0954-909e-4cc3ad4e7ec7>
2011-11-01 15:35:04 +0000

    Change $.shutdown() to use .Shutdown() instead of .Reboot()

--- trunk/jq4a.js
+++ trunk/jq4a.js
@@ -120,7 +120,7 @@
 		var opsys = GetObject("winmgmts:{(Shutdown)}//./root/cimv2").ExecQuery("select * from Win32_OperatingSystem where Primary=true");
 		for(var enumItems = new Enumerator(opsys); !enumItems.atEnd(); enumItems.moveNext())
 		{
-			enumItems.item().Reboot();
+			enumItems.item().Shutdown();
 		}
 	}
   $.base64Encode = function(input) {
